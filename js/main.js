dfafbb8b22438b8eae7a24f8466dbe95ef1b8488
Alessandro Caiti <acaiti@hotmail.it>
2020-03-05 16:54:39 +0100

    Passi avanti

--- js/main.js
+++ js/main.js
@@ -1,3 +1,54 @@
 $(document).ready(function() {
 
+    $("#invia").click(function() {
+        var messaggio = $("#msg").val();
+        $("#msg").val("");
+        console.log(messaggio);
+        var msgInviato = $(".template .user-msg").clone();
+        console.log(msgInviato);
+        msgInviato.find(".msg-txt").text(messaggio);
+        msgInviato.find(".txt-time").text(oraInvio());
+        $(".messanger-container").append(msgInviato);
+    });
+
+
+    $("#invia").click(function() {
+        setTimeout(function () {
+            var msgInviato = $(".template .speaker-msg").clone();
+            msgInviato.find(".msg-txt").text("Ok");
+            msgInviato.find(".txt-time").text(oraInvio());
+            $(".messanger-container").append(msgInviato);
+        }, 1000);
+    });
+
+
+    $("#src").keyup(function(event) {
+        var carattereFiltro = $(this).val().toLowerCase();
+        $(".contact-menu h4").each(function() {
+            if ($(this).text().toLowerCase().includes(carattereFiltro)) {
+                $(this).parentsUntil(".contact-menu").show();
+            } else {
+                $(this).parentsUntil(".contact-menu").hide();
+            }
+        });
+    });
+
+
+    function oraInvio() {
+        var date = new Date();
+        var h = date.getHours();
+        var m = addZero(date.getMinutes());
+        var time = (h + ":" + m);
+        return time;
+    }
+
+    function addZero(i) {
+      if (i < 10) {
+        i = "0" + i;
+      }
+      return i;
+    }
+
+
+
 });
